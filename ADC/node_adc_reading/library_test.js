918e1db36b34eaadcd42003b8f542dae5e9a2297
ekinakyurek <akyurekekin@gmail.com>
2016-10-14 11:03:22 +0000

    Wrting text file for electron .config

--- ADC/node_adc_reading/library_test.js
+++ ADC/node_adc_reading/library_test.js
@@ -11,13 +11,33 @@ var gesture_recognizer = new gesture(1,0,'4096','860')
 //Event handler functions
 ee.on("swipe-up", function (data) {
     console.log("swipe_up gesture is called, velocity:" + data);
-});
+    var fs = require('fs');
+    var path = '/home/pi/.config/electron-quick-start/test.json';
+    fs.writeFile(path, data, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+    }); 
+}); 
+
 ee.on("swipe-down", function (data) {
     console.log("swipe_down gesture is called, velocity:" + data);
 });
+
 ee.on("point-detection", function (data) {
     console.log("point detected at point:" + data);
+    var fs = require('fs');
+    var path = '/home/pi/.config/electron-quick-start/test.json';
+    fs.writeFile(path, data, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+    });
 });
 
 
-gesture_recognizer.startReading(ee);
+gesture_recognizer.startReading(ee)
