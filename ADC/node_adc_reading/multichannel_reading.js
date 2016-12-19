25727f3e4cca08b7b3c2c2bb698a8e0b79702cf0
ekinakyurek <akyurekekin@gmail.com>
2016-12-19 12:45:13 +0000

    Pazartesi oglen

--- ADC/node_adc_reading/multichannel_reading.js
+++ ADC/node_adc_reading/multichannel_reading.js
@@ -6,7 +6,7 @@ var gesture = require("./node_modules/gesture")
 var EventEmitter = require("events").EventEmitter;
 //create eventEmitter and gesture_recognizer objects.
 var ee = new EventEmitter();
-var gesture_recognizer = new gesture(0,0,6144,'3300')
+var gesture_recognizer = new gesture(0,0,4096,'3300')
 
 //Event handler functions
 ee.on("swipe-up,0", function (data) {
--- gui/main.js
+++ gui/main.js
@@ -4,7 +4,7 @@ const electron = require('electron')
 var gesture = require("./node_modules/gesture")
 var EventEmitter = require("events").EventEmitter;
 var ee = new EventEmitter();
-var gesture_recognizer = new gesture(0,0, 6144, '3300')
+var gesture_recognizer = new gesture(0,0, 4096, '3300')
 
 
 // Module to control application life.
